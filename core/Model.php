d0ca9c6dceda65ec2b56301bab0ef26e65990e83
elhac <elhachimihajar73@gmail.com>
2026-06-17 12:22:23 +0200

    Fix connexion BDD dans Model

--- core/Model.php
+++ core/Model.php
@@ -1,11 +1,24 @@
 <?php
-require_once __DIR__ . '/../config/database.php';
-
 class Model {
     protected $pdo;
 
     public function __construct() {
-        global $pdo;
-        $this->pdo = $pdo;
+        $host = 'node.solyzon.com';
+        $port = '3307';
+        $dbname = 'escapegame_G5B';
+        $username = 'escapegame_G5B';
+        $password = 'JYm5co*JAp..K(U]';
+
+        try {
+            $this->pdo = new PDO(
+                "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8",
+                $username,
+                $password
+            );
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        } catch(PDOException $e) {
+            die("Erreur de connexion BDD : " . $e->getMessage());
+        }
     }
 }
