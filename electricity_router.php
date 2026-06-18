c5084885b41313383a3772a0acbf7102e7e26941
elhac <elhachimihajar73@gmail.com>
2026-06-18 12:10:24 +0200

    Fix suppress PHP errors avant JSON

--- electricity_router.php
+++ electricity_router.php
@@ -1,5 +1,8 @@
 <?php
-session_start();
+error_reporting(0);
+ini_set('display_errors', 0);
+
+if (session_status() === PHP_SESSION_NONE) session_start();
 require_once __DIR__ . '/core/Router.php';
 $router = new Router();
 $router->dispatch();
